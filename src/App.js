c2b30d87dac34e510eb2e40bcfa4c48843f2e189
SongLong <songlong2205@gmail.com>
2022-06-04 21:56:49 +0700

    presentational and container components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import { Component } from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './Component/MenuComponent';
+import Main from './Component/MainComponent';
 import './App.css';
-import { DISHES } from './share/dishes';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    }
-  }
 
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className='container'>
-            <NavbarBrand href='/'>Ristorance Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Main />
       </div>
     );
   }
--- src/Component/DishDetailComponent.js
+++ src/Component/DishDetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 class DishDetail extends Component {
 
@@ -26,7 +26,7 @@ class DishDetail extends Component {
                         return (
                             <li key={comment.id}>
                                 <p>{comment.comment}</p>
-                                <p>--{comment.author}, {comment.date}</p>
+                                <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                             </li>
                         );
                     })}
